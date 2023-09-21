88bcdc94c6fb24ed45a7adf1046c52c1c87257db
kkkwp <chxrryda@gmail.com>
2023-09-21 16:13:23 +0900

    라우터 적용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/reset.css";
 import "./styles/global.css";
-import TopNav from "./components/TopNav/TopNav";
 import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="layout">
-      <TopNav />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<div>페이지를 찾을 수 없습니다</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
