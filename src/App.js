324746e48c27402613566a33b149fb1f289b3e10
kkkwp <chxrryda@gmail.com>
2023-09-21 11:37:58 +0900

    상단바(TopNav) 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles/reset.css";
 import "./styles/global.css";
+import TopNav from "./components/TopNav/TopNav";
 
 function App() {
   return (
     <div className="layout">
-      Hello World!
+      <TopNav />
     </div>
   );
 }
